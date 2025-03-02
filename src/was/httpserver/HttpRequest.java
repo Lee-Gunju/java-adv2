54fb91c5ed4632be9570ab2df3ae690fa288be44
1155476 <hanyd1012@hyundai-autoever.com>
2025-03-02 13:01:31 +0900

    http webserver

--- src/was/httpserver/HttpRequest.java
+++ src/was/httpserver/HttpRequest.java
@@ -3,9 +3,11 @@ package was.httpserver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+
+import static java.nio.charset.StandardCharsets.*;
+import static util.MyLogger.log;
 
 public class HttpRequest {
 
@@ -17,40 +19,33 @@ public class HttpRequest {
     public HttpRequest(BufferedReader reader) throws IOException {
         parseRequestLine(reader);
         parseHeaders(reader);
-        // message body는 이후에 처리
+        parseBody(reader); // 추가
     }
-
-    // GET /search?q=hello HTTP/1.1
-    // Host : localhost:12345
 
     private void parseRequestLine(BufferedReader reader) throws IOException {
         String requestLine = reader.readLine();
         if (requestLine == null) {
-            throw new IOException("Invalid request line");
+            throw new IOException("EOF: No request line received");
         }
         String[] parts = requestLine.split(" ");
         if (parts.length != 3) {
-            throw new IOException("Invalid request line " + requestLine);
+            throw new IOException("Invalid request line: " + requestLine);
         }
 
         method = parts[0];
-        // /search?q=hello
         String[] pathParts = parts[1].split("\\?");
         path = pathParts[0];
 
         if (pathParts.length > 1) {
             parseQueryParameters(pathParts[1]);
         }
-
-
     }
 
     private void parseQueryParameters(String queryString) {
-        String[] split = queryString.split("&");
-        for (String param : split) {
-            String[] keyvalue = param.split("=");
-            String key = URLDecoder.decode(keyvalue[0], StandardCharsets.UTF_8);
-            String value = keyvalue.length > 1 ? URLDecoder.decode(keyvalue[1], StandardCharsets.UTF_8) : "";
+        for (String param : queryString.split("&")) {
+            String[] keyValue = param.split("=");
+            String key = URLDecoder.decode(keyValue[0], UTF_8);
+            String value = keyValue.length > 1 ? URLDecoder.decode(keyValue[1], UTF_8) : "";
             queryParameters.put(key, value);
         }
     }
@@ -60,8 +55,28 @@ public class HttpRequest {
         while (!(line = reader.readLine()).isEmpty()) {
             String[] headerParts = line.split(":");
             // trim() 앞 뒤 공백 제거
-            headers.put(headerParts[0].trim(), headerParts[1]);
+            headers.put(headerParts[0].trim(), headerParts[1].trim());
+        }
+    }
 
+    // 추가
+    private void parseBody(BufferedReader reader) throws IOException {
+        if (!headers.containsKey("Content-Length")) {
+            return;
+        }
+
+        int contentLength = Integer.parseInt(headers.get("Content-Length"));
+        char[] bodyChars = new char[contentLength];
+        int read = reader.read(bodyChars);
+        if (read != contentLength) {
+            throw new IOException("Fail to read entire body. Expected " + contentLength + " bytes, but read " + read);
+        }
+        String body = new String(bodyChars);
+        log("HTTP Message Body: " + body);
+
+        String contentType = headers.get("Content-Type");
+        if ("application/x-www-form-urlencoded".equals(contentType)) {
+            parseQueryParameters(body);
         }
     }
 
